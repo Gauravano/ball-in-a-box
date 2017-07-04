46fbfe5e24febd9ca6a8fa1e291af6efceac09eb
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-04 19:28:31 +0530

    image 1 set

--- js_ball.js
+++ js_ball.js
@@ -1,14 +1,58 @@
 
+
 var ball = document.getElementById('ball');
 
+ball.style.top = '0px';
+ball.style.left = '0px';
+
+var number ;
+
 function boxClick(event){
-	console.log(event.clientX);
-	console.log(event.clientY);
+	console.log("x"+ event.clientX);
+	console.log("y"+ event.clientY);
 
-	ball.style.top = event.clientY + 'px'
-	ball.style.left = event.clientX +'px'
+	var newTop = event.clientY;
+	var newLeft = event.clientX;
 
-	console.log(" box is clicked ");
+	var oldTop = parseFloat(ball.style.top.slice(0,-2));
+	var oldLeft = parseFloat(ball.style.left.slice(0,-2));
+
+	var deltaTop = (newTop - oldTop)/100.0;
+	var deltaLeft = (newLeft - oldLeft)/100.0;
+
+
+	if(number != null){
+		console.log("cut between");
+		clearInterval(number);
+	}
+
+	number = setInterval(function(){
+
+	var currentTop = parseFloat(ball.style.top.slice(0,-2));
+	var currentLeft = parseFloat(ball.style.left.slice(0,-2));
+
+	console.log(currentTop);
+	console.log(newTop);
+	
+	mod = currentTop - newTop;
+	if (mod < 0){
+		mod = -1 * mod;
+	}
+
+	if(mod < 0.1){
+		clearInterval(number);
+	}
+
+
+
+	tempTop =(currentTop + deltaTop) ;
+	tempLeft = (currentLeft + deltaLeft) 
+
+
+
+	ball.style.top = tempTop + 'px';
+	ball.style.left = tempLeft + 'px' ;
+	},5);
 
 };
 
